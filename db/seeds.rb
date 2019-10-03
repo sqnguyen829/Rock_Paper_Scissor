10c240a9abd063584f74849265f7c37db21c3ce7
github username <github email address>
2019-10-03 13:45:39 -0500

    small change to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,12 @@ User.destroy_all
 Status.destroy_all
 Scoreboard.destroy_all
 
-u1 = User.create(name: "Tom Turner",status: "chicken", choice: "Rock")
+u1 = User.create(name: "Tom Turner")
 
-s1 = Status.create(name:"Robot One", status: "chicken", choice: "Paper")
+s1 = Status.create(status: "Egg")
+s2 = Status.create(status: "Chicken")
+s3 = Status.create(status: "Raptor")
+s4 = Status.create(status: "OVERLORD")
 
 Scoreboard.create(user_id: u1.id, status_id: s1.id, streak: 10)
 
